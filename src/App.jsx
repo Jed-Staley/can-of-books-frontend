6e35bd4bdf9129a5b03c66b7c293b2d036c9f565
Amanda <amandamarquez27@icloud.com>
2024-04-09 16:15:33 -0700

    Added fronend of books

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import BestBooks from './BestBooks';
+import About from './About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  Link // Import Link component from react-router-dom
 } from "react-router-dom";
 
 class App extends React.Component {
@@ -14,14 +16,26 @@ class App extends React.Component {
     return (
       <>
         <Router>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+              <Link to="/" className="navbar-brand">Home</Link>
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link to="/about" className="nav-link">About</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
           <Header />
           <Routes>
             <Route 
               exact path="/"
               element={<BestBooks />}
-            >
-            </Route>
-            {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
+            />
+            <Route 
+              path="/about"
+              element={<About />}
+            />
           </Routes>
           <Footer />
         </Router>
